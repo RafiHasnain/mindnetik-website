799d51991db285a661c61b602216454ae15bd51a
Rafi Hasnain <rafihasnain02@gmail.com>
2025-07-31 08:55:35 +0600

    Nav bar resize

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -71,7 +71,7 @@ const Navigation = () => {
             : "bg-transparent"
         }`}
       >
-        <div className="max-w-7xl mx-auto px-4 py-5 sm:px-6 lg:px-8">
+        <div className="max-w-full mx-10 px-4 py-5 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-20 ">
             {/* Logo */}
             <Link href="/" className="flex items-center space-x-3 group ">
